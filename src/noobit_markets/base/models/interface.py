740bd739eedc0712e2f007c30733147648ae0a6c
maxima-us <maximousse@protonmail.com>
2020-08-25 19:23:21 +0400

    update interface

--- src/noobit_markets/base/models/interface.py
+++ src/noobit_markets/base/models/interface.py
@@ -1,10 +1,21 @@
 import typing
 import asyncio
 
+import pydantic
+
 from noobit_markets.base.models.rest import endpoints
 from noobit_markets.base.models.frozenbase import FrozenBaseModel
 from noobit_markets.base import ntypes
-from noobit_markets.base.models.rest.response import NoobitResponseOhlc
+
+from noobit_markets.exchanges.kraken.rest.auth import Auth
+
+# response models
+from noobit_markets.base.models.rest.response import (
+    NoobitResponseOhlc,
+    NoobitResponseOrderBook,
+    NoobitResponseSymbols,
+    NoobitResponseBalances
+)
 
 
 
@@ -19,16 +30,69 @@ class _PublicInterface(FrozenBaseModel):
             ntypes.SYMBOL_TO_EXCHANGE,
             ntypes.SYMBOL_FROM_EXCHANGE,
             ntypes.TIMEFRAME,
-            typing.Callable
+            typing.Callable,
+            pydantic.AnyHttpUrl,
+            str
         ],
         # return type
         NoobitResponseOhlc
+    ]
+
+    orderbook: typing.Callable[
+        #argument types
+        [
+            asyncio.BaseEventLoop,
+            ntypes.CLIENT,
+            ntypes.SYMBOL,
+            ntypes.SYMBOL_TO_EXCHANGE,
+            ntypes.SYMBOL_FROM_EXCHANGE,
+            ntypes.DEPTH,
+            typing.Callable,
+            pydantic.AnyHttpUrl,
+            str
+        ],
+        # return type
+        NoobitResponseOrderBook
+    ]
+
+    symbols: typing.Callable[
+        #argument types
+        [
+            asyncio.BaseEventLoop,
+            ntypes.CLIENT,
+            typing.Callable,
+            pydantic.AnyHttpUrl,
+            str
+        ],
+        # return type
+        NoobitResponseSymbols
+    ]
+
+
+
+class _PrivateInterface(FrozenBaseModel):
+
+    balances: typing.Callable[
+        #argument types
+        [
+            asyncio.BaseEventLoop,
+            ntypes.CLIENT,
+            ntypes.SYMBOL_TO_EXCHANGE,
+            Auth,
+            typing.Callable,
+            pydantic.AnyHttpUrl,
+            str
+        ],
+        # return type
+        # FIXME should be result
+        NoobitResponseBalances
     ]
 
 
 class RestInterface(FrozenBaseModel):
 
     public: _PublicInterface
+    private: _PrivateInterface
 
 
 
--- src/noobit_markets/exchanges/kraken/interface.py
+++ src/noobit_markets/exchanges/kraken/interface.py
@@ -1,12 +1,24 @@
 from noobit_markets.base.models.interface import ExchangeInterface
 
+
+# private endpoints
+from noobit_markets.exchanges.kraken.rest.private.balances.get import get_balances_kraken
+
+# public endpoints
 from noobit_markets.exchanges.kraken.rest.public.ohlc.get import get_ohlc_kraken
+from noobit_markets.exchanges.kraken.rest.public.symbols.get import get_symbols
+from noobit_markets.exchanges.kraken.rest.public.orderbook.get import get_orderbook_kraken
 
 
 KRAKEN = ExchangeInterface(**{
     "rest": {
         "public": {
-            "ohlc": get_ohlc_kraken
+            "ohlc": get_ohlc_kraken,
+            "orderbook": get_orderbook_kraken,
+            "symbols": get_symbols,
+        },
+        "private": {
+            "balances": get_balances_kraken
         }
     }
 })
