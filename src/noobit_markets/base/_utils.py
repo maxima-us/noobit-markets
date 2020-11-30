0e35baafb7efad8d11b509ec0ea9bdbeaf11ad06
maxima-us <maximousse@protonmail.com>
2020-11-30 16:12:59 +0400

    meh

--- src/noobit_markets/base/_utils.py
+++ src/noobit_markets/base/_utils.py
@@ -15,60 +15,69 @@ from noobit_markets.base.models.frozenbase import FrozenBaseModel
 import pydantic
 
 import stackprinter
+
 stackprinter.set_excepthook(style="darkbg2")
 
 
 from noobit_markets.base.models.rest.request import NoobitRequestOhlc
 from noobit_markets.base.models.rest.response import NoobitResponseOhlc
-from noobit_markets.exchanges.kraken.rest.public.ohlc.response import make_kraken_model_ohlc
-from noobit_markets.exchanges.kraken.rest.public.ohlc.request import KrakenRequestOhlc
+from noobit_markets.exchanges.kraken.rest.public.ohlc import (
+    make_kraken_model_ohlc,
+    KrakenRequestOhlc,
+)
 
 from noobit_markets.base.models.result import Result, Ok, Err
 from pydantic.error_wrappers import ValidationError
 from typing_extensions import Literal
 
 
-KrakenResponseOhlc = make_kraken_model_ohlc("XBT-USD", {"XBT-USD": "XXBTZUSD"})
+# ============================================================
+# MODEL USED TO TEST DECORATOR LATER
+# ============================================================
 
 
+KrakenResponseOhlc = make_kraken_model_ohlc("XBT-USD", lambda x: {"XBT-USD": "XXBTZUSD"}.get(x))
+
+
+class KrakenFullResp(FrozenBaseModel):
+    error: typing.Tuple[str, ...]
+    result: typing.Optional[KrakenResponseOhlc]
+
+
+#! useless
 class HttpxReq(pydantic.BaseModel):
-    
+
     method: Literal["get", "post"]
-    
+
     url: pydantic.AnyHttpUrl
     # headers: typing.Optional[httpx._types.HeaderTypes] = None
     # content: typing.Optional[httpx._types.RequestContent] = None
-    
+
     # for private requests
     # data: typing.Optional[httpx._types.RequestData] = None
-    
+
     # for public requests
     params: typing.Optional[typing.Any] = None
-    
+
     # json: typing.Optional[typing.Any] = None
     # cookies: typing.Optional[httpx._types.CookieTypes] = None
 
 
+# ? ============================================================
+#! GENERAL PURPOSE DECORATOR
+# ? ============================================================
 
 
-def validate_request(
-        get_result: typing.Callable,
-        input: pydantic.BaseModel,
-        output: pydantic.BaseModel
-    ):
-    
+def validate_request(input: pydantic.BaseModel, output: pydantic.BaseModel):
     def decorator(func: types.CoroutineType):
         @wraps(func)
-        async def wrapper(
-            *args,
-            **kwargs
-            ):
-            
+        async def wrapper(*args, **kwargs):
+
             print("Wrapped func is :", func)
-            
-            if args: print("Warning: Limit use of args to <Client>")
-            
-            
+
+            if args:
+                print("Warning: Limit use of args to <Client>")
+
             # valid req params against input model
             # validation and serialization only for KWargs
             try:
@@ -78,76 +87,72 @@ def validate_request(
                 return Err(e)
             except Exception as e:
                 raise e
-                
+
             # get response content
             try:
                 print("Valid params are :", params)
-                
-                resp =  await func(*args, **params)
-                # print("Response is: ", resp.__dict__)
-                # result = get_result(json.loads(resp.content))
+
+                resp = await func(*args, **params)
 
                 # different clients will define json as either sync or async method
                 if inspect.iscoroutinefunction(resp.json):
-                    result = get_result(await resp.json())
+                    result = await resp.json()
                 else:
-                    result = get_result(resp.json())
+                    result = resp.json()
                 valid_content = output(**result)
                 return Ok(resp)
-                
+
             except ValidationError as e:
                 return Err(e)
             except Exception as e:
                 raise e
-            
+
         return wrapper
-    
+
     return decorator
 
 
+# ? ============================================================
+#! OHLC REQUEST CORO
+# ? ============================================================
 
 
-
-
-
-   
-@validate_request(get_result=lambda x: x["result"], input=KrakenRequestOhlc, output=KrakenResponseOhlc) 
+# @validate_request(get_result=lambda x: x["result"], input=KrakenRequestOhlc, output=KrakenFullResp)
+@validate_request(input=KrakenRequestOhlc, output=KrakenFullResp)
 async def get_ohlc_kraken(client, *, pair, interval):
+    """Necessary to explicitely decalre pair and interval as kwargs here"""
+
     url = "https://api.kraken.com/0/public/OHLC"
-    
-    
-    payload = {
-        "pair": pair,
-        "interval": interval
-    }
-    
-    resp = await client.get(
-        url=url, 
-        params=payload
-    )
+
+    payload = {"pair": pair, "interval": interval}
+
+    resp = await client.get(url=url, params=payload)
     # print(resp)
     return resp
-        
 
+
+
+
+# ? ============================================================
+#! TEST CASES
+# ? ============================================================
 
 
 async def get_httpx():
     async with httpx.AsyncClient() as client:
-        result = await get_ohlc_kraken(
-            client, 
-            pair="XXBTZUSD",
-            interval=60
-            )
+        result = await get_ohlc_kraken(client, pair="XXBTZUSD", interval=60)
         if result.is_ok():
             # httpx client.json is a standard method
             json = result.value.json()
-            print(json["result"]["XXBTZUSD"][0])
+            print("First Candle :", json["result"]["XXBTZUSD"][0])
+            print("Status code :", result.value.status_code)
+            print("Request :", result.value.request)
 
         if result.is_err():
-            print(result)
-        
-# asyncio.run(get_httpx())
+            print(result.value)
 
+
+# asyncio.run(get_httpx())
 
 
 async def get_aiohttp():
@@ -156,59 +161,56 @@ async def get_aiohttp():
             session,
             pair="XXBTZUSD",
             #! fail on purpose
-            interval="1H"
+            interval=60,
         )
         if result.is_ok():
             # aiohttp client.json is a coro
             json = await result.value.json()
             print(json["result"]["XXBTZUSD"][0])
 
+            # will have different property names than httpx for similar data
+            print("Status code :", result.value.status)
+            print("Request :", result.value.request_info)
+
         if result.is_err():
-            print(result)
-    
+            print(result.value)
+
+
 # asyncio.run(get_aiohttp())
 
 
-
-
-
-# ============================================================
-# ============================================================
-# ============================================================
-# ---- NOOBIT APPLICATION ------------------------------------
-# ============================================================
-# ============================================================
-# ============================================================
-
+#! ============================================================
+#! ============================================================
+#! ============================================================
+#! ---- NOOBIT APPLICATION ------------------------------------
+#! ============================================================
+#! ============================================================
+#! ============================================================
+"""Totally overkill
+"""
 
 from pyrsistent import pmap, s
+
 # from noobit_markets.exchanges.kraken.rest.public.ohlc.request import parse_request_ohlc
 # from noobit_markets.exchanges.kraken.rest.public.ohlc.response import parse_result_data_ohlc
 from noobit_markets.base import ntypes, mappings
 
 
-def parse_request_ohlc(
-        valid_request: NoobitRequestOhlc
-    ) -> pmap:
-
+def parse_request_ohlc(valid_request: NoobitRequestOhlc) -> pmap:
 
     payload = {
-        "pair": valid_request.symbol_mapping[valid_request.symbol],
+        "pair": valid_request.symbol_mapping(valid_request.symbol),
         "interval": mappings.TIMEFRAME[valid_request.timeframe],
         # noobit ts are in ms vs ohlc kraken ts in s
-        "since": valid_request.since * 10**-3 if valid_request.since else None
+        "since": valid_request.since * 10 ** -3 if valid_request.since else None,
     }
-
 
     return pmap(payload)
 
 
 def parse_result_data_ohlc(
-        response: pydantic.BaseModel, 
-        symbol: ntypes.SYMBOL,
-        symbol_mapping
-    ) -> typing.Tuple[pmap]:
-
+    response: pydantic.BaseModel, symbol: ntypes.SYMBOL, symbol_mapping
+) -> typing.Tuple[pmap]:
 
     response_content = getattr(response, "result", None)
     response_error = getattr(response, "error", None)
@@ -216,51 +218,49 @@ def parse_result_data_ohlc(
     if response_error:
         return Err(response_error)
 
-    ohlc = getattr(response_content, symbol_mapping[symbol])
+    ohlc = getattr(response_content, symbol_mapping(symbol))
     last = getattr(response_content, "last")
     parsed_ohlc = [_single_candle(data, symbol) for data in ohlc]
 
-    return Ok({"ohlc": parsed_ohlc, "last":last})
+    return Ok({"ohlc": parsed_ohlc, "last": last})
 
 
-def _single_candle(
-        data: tuple,
-        symbol: ntypes.SYMBOL
-    ) -> pmap:
+def _single_candle(data: tuple, symbol: ntypes.SYMBOL) -> pmap:
 
     parsed = {
         "symbol": symbol,
-        "utcTime": data[0]*10**3,
+        "utcTime": data[0] * 10 ** 3,
         "open": data[1],
         "high": data[2],
         "low": data[3],
         "close": data[4],
         "volume": data[6],
-        "trdCount": data[7]
+        "trdCount": data[7],
     }
 
     return pmap(parsed)
 
 
-def validate_input(noobit_input: pydantic.BaseModel, exchange_input: pydantic.BaseModel, parser: typing.Callable):
-    
+def validate_input(
+    noobit_input: pydantic.BaseModel,
+    exchange_input: pydantic.BaseModel,
+    parser: typing.Callable,
+):
     def decorator(func: types.CoroutineType):
         @wraps(func)
-        async def wrapper(
-            *args,
-            **kwargs
-            ):
-            
-            if args: print("Warning: Limit use of args to <Client>")
+        async def wrapper(*args, **kwargs):
+
+            if args:
+                print("Warning: Limit use of args to <Client>")
 
             try:
-                valid_noobit_req = noobit_input(**kwargs) 
+                valid_noobit_req = noobit_input(**kwargs)
                 parsed = parser(valid_noobit_req)
             except ValidationError as e:
                 return Err(e)
             except Exception as e:
                 raise e
-            
+
             try:
                 valid_exch_req = exchange_input(**parsed)
                 params = valid_exch_req.dict(exclude_none=True)
@@ -272,30 +272,24 @@ def validate_input(noobit_input: pydantic.BaseModel, exchange_input: pydantic.Ba
                 raise e
 
         return wrapper
-    
+
     return decorator
 
 
-
-
-#? needs to be the "most outer" decorator if we also validate input
+# ? needs to be the "most outer" decorator if we also validate input
 def validate_output(
-        target: typing.Callable,
-        exchange_output: pydantic.BaseModel,
-        noobit_output: pydantic.BaseModel,
-        parser: typing.Callable
-    ):
-    
+    target: typing.Callable,
+    exchange_output: pydantic.BaseModel,
+    noobit_output: pydantic.BaseModel,
+    parser: typing.Callable,
+):
     def decorator(func: types.CoroutineType):
         @wraps(func)
-        async def wrapper(
-            *args,
-            **kwargs
-            ):
-            
+        async def wrapper(*args, **kwargs):
+
             try:
                 # print("Valid params are :", **kwargs)
-                resp =  await func(*args, **kwargs)
+                resp = await func(*args, **kwargs)
 
                 # if we chain it with a validate_input decorator it will return a Result
                 # if isinstance(resp, Result):
@@ -309,7 +303,7 @@ def validate_output(
                     result = target(await resp.json())
                 else:
                     result = target(resp.json())
-               
+
                 valid_exch_content = exchange_output(**result)
             except ValidationError as e:
                 return Err(e)
@@ -318,61 +312,65 @@ def validate_output(
 
             try:
                 parsed = parser(
-                    valid_exch_content, 
+                    valid_exch_content,
                     symbol=kwargs.get("symbol", None),
-                    symbol_mapping=kwargs.get("symbol_mapping")
+                    symbol_mapping=kwargs.get("symbol_mapping"),
                 )
 
                 if parsed.is_err():
                     return parsed
 
-                #? what happens when we need to pass a list (ex model(trades=parsed)
+                # ? what happens when we need to pass a list (ex model(trades=parsed)
                 valid_noobit_content = noobit_output(**parsed.value)
                 return Ok(valid_noobit_content)
             except ValidationError as e:
                 return Err(e)
             except Exception as e:
-                raise 
-            
+                raise
+
         return wrapper
-    
+
     return decorator
 
 
 
-class KrakenFullResp(FrozenBaseModel):
-    error: typing.Tuple[str, ...]
-    result: typing.Optional[KrakenResponseOhlc]
+#? ============================================================
+# ! TEST
+#? ============================================================
 
-
-
-#? how to we target the symbol / also this will depend on the request (sometimes not indexed)
-@validate_output(lambda x: x, KrakenFullResp, NoobitResponseOhlc, parse_result_data_ohlc)
-@validate_input(NoobitRequestOhlc, KrakenRequestOhlc, parse_request_ohlc)
-async def full_ohlc_req(client, *args, **kwargs):
+# ? how to we target the symbol / also this will depend on the request (sometimes not indexed)
+@validate_output(
+    target=lambda x: x,
+    exchange_output=KrakenFullResp,
+    noobit_output=NoobitResponseOhlc,
+    parser=parse_result_data_ohlc
+)
+@validate_input(
+    noobit_input=NoobitRequestOhlc,
+    exchange_input=KrakenRequestOhlc,
+    parser=parse_request_ohlc
+)
+async def full_ohlc_req(client, **kwargs):
     url = "https://api.kraken.com/0/public/OHLC"
-    
+
     payload = {
         "pair": kwargs.get("pair", None),
-        "interval": kwargs.get("interval", None)
+        "interval": kwargs.get("interval", None),
     }
-    
-    resp = await client.get(
-        url=url, 
-        params=payload
-    )
+
+    resp = await client.get(url=url, params=payload)
     # print(resp)
     return resp
-            
+
 
 async def get_full_httpx():
     async with httpx.AsyncClient() as client:
         result = await full_ohlc_req(
-            client, 
-            symbol_mapping={"XBT-USD": "XXBTZUSD"},
+            client,
+            symbol_mapping=lambda x: {"XBT-USD": "XXBTZUSD"}.get(x),
             symbol="XBT-USD",
             timeframe="1H",
-            )
+        )
         if result.is_ok():
             # httpx client.json is a standard method
             json = result.value
@@ -381,5 +379,6 @@ async def get_full_httpx():
 
         if result.is_err():
             print(result)
-        
+
+
 asyncio.run(get_full_httpx())
